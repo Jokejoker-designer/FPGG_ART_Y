e8d550d4fa515d568a62c219cbda02cdab27e476
Jokejoker-designer <phantrananhquan2306@gmail.com>
2026-09-02 04:23:05 +0700

    P2-WDMA-RELEASE-CDC-AUDIT-03: close u_wdma_rel_sync CDC-10. candidate_logic=0 free=427 WNS=+1.276 bit 6975AB75. PROGRAM=NO.

---
+++ results/A7-NATIVE-GRAPH/GROK-ORCH-00/P2-WDMA-RELEASE-CDC-AUDIT-03/run_p2_wdma_release_cdc_audit_03.bat
@@ -0,0 +1,7 @@
+@echo off
+setlocal
+set XILINXD_LICENSE_FILE=D:\Xilinx\licenses\vivado_basic.lic
+set BAG=%~dp0
+echo P2-WDMA-RELEASE-CDC-AUDIT-03 PHYS=4 LABEL=MINHEAP PROGRAM=NO
+call C:\2026.1\Vivado\bin\vivado.bat -mode batch -notrace -source "%BAG%build_p2_wdma_release_cdc_audit_03.tcl" -log "%BAG%vivado_physical.log" -journal "%BAG%vivado_physical.jou"
+exit /b %ERRORLEVEL%
